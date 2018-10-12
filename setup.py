a9ea05c1c12430e41cfbe267d5eed765a1c4e323
Tom Gillespie <tgbugs@gmail.com>
2018-10-11 21:30:42 -0700

    setup.py version bump and include new files

--- setup.py
+++ setup.py
@@ -4,15 +4,24 @@ from setuptools import setup
 
 files = [
     'ontquery/__init__.py',
+    'ontquery/exceptions.py',
+    'ontquery/plugin.py',
+    'ontquery/plugins/__init__.py',
+    'ontquery/plugins/services.py',
+    'ontquery/query.py',
+    'ontquery/services.py',
+    'ontquery/terms.py',
+    'ontquery/utils.py',
 ]
 
 try:
     os.mkdir('export')
+    os.mkdir('export/plugins')
     for f in files:
         shutil.copyfile(f, f.replace('ontquery','export'))
     setup(
         name='ontquery',
-        version='0.0.2',
+        version='0.0.3',
         description='a framework querying ontology terms',
         long_description=' ',
         url='https://github.com/tgbugs/ontquery',
