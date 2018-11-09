999d5e7293e1d956a5f74efa3b965d8775ab961a
Troy Sincomb <troysincomb@gmail.com>
2018-11-09 14:26:25 -0800

    add testing for the service usage

--- test/test_interlex_client.py
+++ test/test_interlex_client.py
@@ -2,14 +2,19 @@ import os
 import pytest
 import random
 from ontquery.plugins.interlex_client import InterLexClient
+from ontquery.plugins.services import InterLexRemote
 import string
 
 
-sci = InterLexClient(
+ilx_cli = InterLexClient(
     api_key = os.environ.get('INTERLEX_API_KEY'),
     base_url = 'https://beta.scicrunch.org',
 )
 
+services_cli = InterLexRemote(
+    api_key = os.environ.get('INTERLEX_API_KEY'),
+    apiEndpoint = 'https://beta.scicrunch.org',
+)
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -22,7 +27,7 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     ("tmp_123", 'tmp_123'),
 ])
 def test_fix_ilx(test_input, expected):
-    assert sci.fix_ilx(test_input) == expected
+    assert ilx_cli.fix_ilx(test_input) == expected
 
 
 def test_add_raw_entity():
@@ -52,10 +57,10 @@ def test_add_raw_entity():
         ],
     }
 
-    added_entity_data = sci.add_raw_entity(entity.copy())
+    added_entity_data = ilx_cli.add_raw_entity(entity.copy())
     print(added_entity_data)
     # returned value is not identical to get_entity
-    added_entity_data = sci.get_entity(added_entity_data['ilx'])
+    added_entity_data = ilx_cli.get_entity(added_entity_data['ilx'])
     print(added_entity_data)
 
     assert added_entity_data['label'] == entity['label']
@@ -69,9 +74,9 @@ def test_add_raw_entity():
     assert added_entity_data['existing_ids'][0]['curie'] == entity['existing_ids'][0]['curie']
 
     ### ALREADY EXISTS TEST
-    added_entity_data = sci.add_raw_entity(entity.copy())
+    added_entity_data = ilx_cli.add_raw_entity(entity.copy())
     # returned value is not identical to get_entity
-    added_entity_data = sci.get_entity(added_entity_data['ilx'])
+    added_entity_data = ilx_cli.get_entity(added_entity_data['ilx'])
 
     assert added_entity_data['label'] == entity['label']
     assert added_entity_data['type'] == entity['type']
@@ -90,7 +95,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"You need key\(s\): {'label'}"
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
 
     # Invalid keys not wanted
     bad_entity = entity.copy()
@@ -99,7 +104,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"Unexpected key\(s\): {'candy'}"
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
 
     # Invalid superclass ilx_id
     bad_entity = entity.copy()
@@ -108,7 +113,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"Superclass ILX ID: ilx_rgb does not exist in SciCrunch"
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
     bad_entity = entity.copy()
     bad_entity['superclass']['ilx_id'] = 'ilx_0108124'
 
@@ -118,7 +123,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"Synonym not given a literal for label: " + bad_entity['label']
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
     bad_entity['synonyms'][0]['literal'] = bad_entity['synonyms'][0].pop('literals')
 
     # Invalid synonyms -> keys besides literal found
@@ -128,7 +133,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"Extra key\(s\) not recognized in synonyms for label: " + bad_entity['label']
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
     bad_entity['synonyms'][0].pop('literals')
 
     # Invalid existing_ids -> needed not found
@@ -138,7 +143,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"Missing needing key\(s\) in existing_ids for label: " + bad_entity['label']
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
     bad_entity['existing_ids'][0]['iri'] = popped_iri
 
     # Invalid existing_ids -> needed not found
@@ -148,7 +153,7 @@ def test_add_raw_entity():
         SystemExit,
         match=r"Extra keys not recognized in existing_ids for label: " + bad_entity['label']
     ):
-        sci.add_raw_entity(bad_entity)
+        ilx_cli.add_raw_entity(bad_entity)
     bad_entity['existing_ids'][0].pop('iris')
 
 
@@ -177,7 +182,7 @@ def test_add_annotation():
             },
         ],
     }
-    added_entity_data = sci.add_raw_entity(entity.copy())
+    added_entity_data = ilx_cli.add_raw_entity(entity.copy())
 
     annotation = {
         'term_ilx_id': 'ilx_0101431', # brain ILX ID
@@ -185,14 +190,14 @@ def test_add_annotation():
         'annotation_value': 'test_annotation_value',
     }
 
-    added_anno_data = sci.add_annotation(**annotation.copy())
+    added_anno_data = ilx_cli.add_annotation(**annotation.copy())
     assert added_anno_data['id'] != False
     assert added_anno_data['tid'] != False
     assert added_anno_data['annotation_tid'] != False
     assert added_anno_data['value'] == annotation['annotation_value']
 
     # MAKING SURE DUPLICATE STILL RETURNS SAME INFO
-    added_anno_data = sci.add_annotation(**annotation.copy())
+    added_anno_data = ilx_cli.add_annotation(**annotation.copy())
     assert added_anno_data['id'] != False
     assert added_anno_data['tid'] != False
     assert added_anno_data['annotation_tid'] != False
@@ -204,7 +209,7 @@ def test_add_annotation():
         SystemExit,
         match=r"term_ilx_id: ilx_rgb does not exist",
     ):
-        sci.add_annotation(**bad_anno)
+        ilx_cli.add_annotation(**bad_anno)
 
     bad_anno = annotation.copy()
     bad_anno['annotation_type_ilx_id'] = 'ilx_rgb'
@@ -212,4 +217,43 @@ def test_add_annotation():
         SystemExit,
         match=r"annotation_type_ilx_id: ilx_rgb does not exist",
     ):
-        sci.add_annotation(**bad_anno)
+        ilx_cli.add_annotation(**bad_anno)
+
+def test_services():
+    random_label = 'test_' + id_generator(size=12)
+
+    entity = {
+        'label': random_label,
+        'type': 'term', # broken at the moment NEEDS PDE HARDCODED
+        'definition': 'Part of the central nervous system',
+        'comment': 'Cannot live without it',
+        'subThingOf': 'http://uri.interlex.org/base/ilx_0108124', # ILX ID for Orga
+        'synonyms': ['Encephalon', 'Cerebro'],
+        'predicates': {
+            'http://uri.interlex.org/base/tmp_0381624': 'sample_value' # hasDbXref beta ID
+        }
+    }
+    added_entity_data = services_cli.add_entity(**entity.copy())
+    # returned value is not identical to get_entity
+    added_entity_data = ilx_cli.get_entity(added_entity_data['ilx'])
+
+    assert added_entity_data['label'] == entity['label']
+    assert added_entity_data['type'] == entity['type']
+    assert added_entity_data['definition'] == entity['definition']
+    assert added_entity_data['comment'] == entity['comment']
+    assert added_entity_data['superclasses'][0]['ilx'] == entity['subThingOf'].replace('http://uri.interlex.org/base/', '')
+    assert added_entity_data['synonyms'][0]['literal'] == entity['synonyms'][0]
+    assert added_entity_data['synonyms'][1]['literal'] == entity['synonyms'][1]
+
+    ### ALREADY EXISTS TEST
+    added_entity_data = services_cli.add_entity(**entity.copy())
+    # returned value is not identical to get_entity
+    added_entity_data = ilx_cli.get_entity(added_entity_data['ilx'])
+
+    assert added_entity_data['label'] == entity['label']
+    assert added_entity_data['type'] == entity['type']
+    assert added_entity_data['definition'] == entity['definition']
+    assert added_entity_data['comment'] == entity['comment']
+    assert added_entity_data['superclasses'][0]['ilx'] == entity['subThingOf'].replace('http://uri.interlex.org/base/', '')
+    assert added_entity_data['synonyms'][0]['literal'] == entity['synonyms'][0]
+    assert added_entity_data['synonyms'][1]['literal'] == entity['synonyms'][1]
