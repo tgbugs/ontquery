9820081c4dc63a0d57f841f29c0bfd940d05a576
Tom Gillespie <tgbugs@gmail.com>
2019-04-01 05:50:43 -0400

    version bump ahead of release

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ try:
     tests_require = ['pytest', 'pytest-runner', 'rdflib', 'requests']
     setup(
         name='ontquery',
-        version='0.0.7',
+        version='0.0.8',
         description='a framework querying ontology terms',
         long_description=long_description,
         long_description_content_type='text/markdown',
