b65f2fc41c231e448beae2f3dd874a875d8b5e29
Tom Gillespie <tgbugs@gmail.com>
2019-04-29 19:51:04 -0700

    proper fix for edges with bad iris
    ideally this would be fixed upstream ...

--- ontquery/plugins/services.py
+++ ontquery/plugins/services.py
@@ -96,8 +96,10 @@ class SciGraphRemote(OntService):  # incomplete and not configureable yet
             #subjects = set(subject.curie)
             seen = {subject.curie}
             for i, e in enumerate(self.sgg.ordered(subject.curie, edges, inverse=inverse)):
-                if e.startswith('_:'):
-                    continue  # FIXME warn on these ? bnode getting pulled in ...
+                if [v for k, v in e.items() if k != 'meta' and v.startswith('_:')]:
+                    # FIXME warn on these ? bnode getting pulled in ...
+                    # argh ... this is annoying to deal with
+                    continue
                 #print('record number:', i)  # FIXME
                 # FIXME need to actually get the transitive closure, this doesn't actually work
                 #if e[s] in subjects:
